f4c1557a117414bf46a5885cca383e3940390ab4
M.MASIL FAROOQ <masilfarooq6805@gmail.com>
2025-12-05 02:08:00 +0500

    create signUp api route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import userRouter from './routes/user.route.js';
+import cors from 'cors';
+import authRouter from './routes/auth.route.js';
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 mongoose.connect("mongodb://127.0.0.1:27017/mern-estate").then(()=>{
     console.log ('Connected to MongoDB!');
 }).catch((err)=>{
     console.log(err)
 });
 
-const app = express();
+
 
 app.listen(3000,()=>{
     console.log ('server is running on port 3000');
@@ -16,4 +22,5 @@ app.listen(3000,()=>{
 
 
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter);
 
